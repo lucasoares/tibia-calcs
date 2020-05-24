f8e95755422411e9fbe6465616c88323aae944c0
Luiz Miranda <luiz.miranda@dtidigital.com.br>
2020-05-24 16:37:59 -0300

    create Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,34 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <router-link to="/">
+          <h1>Tibia Calc</h1>
+        </router-link>
       </div>
-
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
+      <router-link to="/loot">
+        <v-btn text>
+          <span class="mr-2">Loot Calculator</span>
+        </v-btn>
+      </router-link>
+      <v-btn href="https://github.com/luizcsm/tibia-calc" target="_blank" text>
+        <span class="mr-2">GitHub</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
     //
--- src/views/LootCalculator.vue
+++ src/views/LootCalculator.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Home<h1/>
+    <h1>Loot Calculator<h1/>
   </div>
 </template>
 
